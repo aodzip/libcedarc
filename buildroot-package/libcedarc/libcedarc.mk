f0f4b1c327022f5b541ccb07079dbde981267f27
Aodzip <aodzip@gmail.com>
2020-06-01 12:09:10 +0800

    update makefile

--- buildroot-package/libcedarc/libcedarc.mk
+++ buildroot-package/libcedarc/libcedarc.mk
@@ -9,63 +9,65 @@ LIBCEDARC_DEPENDENCIES =
 LIBCEDARC_INSTALL_STAGING = YES
 LIBCEDARC_INSTALL_TARGET = YES
 LIBCEDARC_AUTORECONF = YES
+LIBCEDARC_ARCHLIB = $(call qstrip,$(BR2_PACKAGE_LIBCEDARC_ARCHLIB))
 LIBCEDARC_CONF_ENV = \
 	CFLAGS="$(TARGET_CFLAGS)" \
-	LDFLAGS="$(TARGET_LDFLAGS) -L$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)"
+	LDFLAGS="$(TARGET_LDFLAGS) -L$(@D)/library/$(LIBCEDARC_ARCHLIB)"
 LIBCEDARC_CONF_OPTS = 
-$(eval $(autotools-package))
 
-mkdir -p "$(TARGET_DIR)/usr/lib/"
-cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libVE.so" "$(TARGET_DIR)/usr/lib/"
-cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libvideoengine.so" "$(TARGET_DIR)/usr/lib/"
+LIBCEDARC_INSTALL_TARGET_CMDS += mkdir -p '$(TARGET_DIR)/usr/lib/';
+LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libVE.so' '$(TARGET_DIR)/usr/lib/';
+LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libvideoengine.so' '$(TARGET_DIR)/usr/lib/';
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_DECODER_AVS),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawavs.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawavs.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_DECODER_H264),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawh264.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawh264.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_DECODER_H265),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawh265.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawh265.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_DECODER_MJPEG),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawmjpeg.so" "$(TARGET_DIR)/usr/lib/"
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawmjpegplus.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawmjpeg.so' '$(TARGET_DIR)/usr/lib/';
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawmjpegplus.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_DECODER_MPEG2),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawmpeg2.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawmpeg2.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_DECODER_MPEG4),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawmpeg4base.so" "$(TARGET_DIR)/usr/lib/"
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawmpeg4dx.so" "$(TARGET_DIR)/usr/lib/"
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawmpeg4h263.so" "$(TARGET_DIR)/usr/lib/"
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawmpeg4normal.so" "$(TARGET_DIR)/usr/lib/"
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawmpeg4vp6.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawmpeg4base.so' '$(TARGET_DIR)/usr/lib/';
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawmpeg4dx.so' '$(TARGET_DIR)/usr/lib/';
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawmpeg4h263.so' '$(TARGET_DIR)/usr/lib/';
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawmpeg4normal.so' '$(TARGET_DIR)/usr/lib/';
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawmpeg4vp6.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_DECODER_VP8),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawvp8.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawvp8.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_DECODER_VP9),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawvp9Hw.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawvp9Hw.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_DECODER_WMV3),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libawwmv3.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libawwmv3.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifeq ($(BR2_PACKAGE_LIBCEDARC_ENCODER),y)
-	cp "$(@D)/library/$(BR2_PACKAGE_LIBCEDARC_ARCHLIB)/libvencoder.so" "$(TARGET_DIR)/usr/lib/"
+	LIBCEDARC_INSTALL_TARGET_CMDS += cp '$(@D)/library/$(LIBCEDARC_ARCHLIB)/libvencoder.so' '$(TARGET_DIR)/usr/lib/';
 endif
 
 ifneq ($(BR2_PACKAGE_LIBCEDARC_OPENMAX),y)
-	rm "$(TARGET_DIR)/usr/lib/libOmxCore.so"
-	rm "$(TARGET_DIR)/usr/lib/libOmxVdec.so"
-	rm "$(TARGET_DIR)/usr/lib/libOmxVenc.so"
+	LIBCEDARC_INSTALL_TARGET_CMDS += rm '$(TARGET_DIR)/usr/lib/libOmxCore.so';
+	LIBCEDARC_INSTALL_TARGET_CMDS += rm '$(TARGET_DIR)/usr/lib/libOmxVdec.so';
+	LIBCEDARC_INSTALL_TARGET_CMDS += rm '$(TARGET_DIR)/usr/lib/libOmxVenc.so';
 endif
+
+$(eval $(autotools-package))
